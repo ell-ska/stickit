144292c0e09244e3e126d20648a6f0e354a29f79
ell-ska <ella@skagerstrom.se>
2023-02-13 14:02:48 +0100

    add sticky and toolbar focus mode

--- src/components/sticky.jsx
+++ src/components/sticky.jsx
@@ -2,15 +2,29 @@ import '../styles/sticky.css'
 
 const Sticky = ({ text, color, id, saveStickyText }) => {
 
+    const handleToolbar = (stickyIsFocused) => {
+
+        const toolbar = document.querySelector('.toolbar')
+
+        if (stickyIsFocused) {
+            toolbar.classList.add('toolbar--active')
+        } else {
+            toolbar.classList.remove('toolbar--active')
+        }
+
+    }
+
     return (
         <div className={'sticky sticky' + color}>
             <textarea
                 className="sticky__text"
                 defaultValue={text}
-                onChange={(e) => {
+                onChange={e => {
                     const inputValue = e.target.value
                     saveStickyText(id, inputValue)
                 }}
+                onFocus={() => handleToolbar(true)}
+                onBlur={() => handleToolbar(false)}
             ></textarea>
         </div>
     )
