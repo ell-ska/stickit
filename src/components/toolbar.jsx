39aa2f0f8b2748df5fabf3046df8038ddfc1b1cd
ell-ska <ella@skagerstrom.se>
2023-02-20 09:54:51 +0100

    remove change text feature for now

--- src/components/toolbar.jsx
+++ src/components/toolbar.jsx
@@ -4,12 +4,12 @@ const Toolbar = ({ deleteSticky, changeStickyColor, stickyColors }) => {
 
     return (
         <div className="toolbar">
-            <div className="text">
+            {/* <div className="text">
                 <button className="text__bold">B</button>
                 <button className="text__italic">I</button>
                 <button className="text__underline">U</button>
                 <button className="text__strikethrough">S</button>
-            </div>
+            </div> */}
             <div className="color">
                 {stickyColors.map(color => {
                     return <button key={color} className={"color__swatch color__swatch" + color} onClick={() => changeStickyColor(color)}></button>
